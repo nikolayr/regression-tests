85ab19566bb57ea58114067da79053f84c69c5f3
Nikolay Ruban <nikolayr@gmail.com>
2015-10-03 03:38:14 +0300

    testing phantomjs

--- Gemfile
+++ Gemfile
@@ -1,10 +1,13 @@
 source 'https://rubygems.org'
 
+# rvm is being used - curl -sSL https://get.rvm.io | bash -s stable --ruby
+
 gem 'cucumber'
 gem 'bundle'
 gem 'capybara'
 gem 'selenium-webdriver'
+gem 'poltergeist'
+gem 'wait_until'
 
-#gem 'poltergeist'
-#gem 'phantomjs'
+#gem 'phantomjs' - loaded through apt-get install
 
---
+++ test/c.rb
@@ -0,0 +1,31 @@
+require 'capybara'
+require 'capybara/poltergeist'
+require 'wait_until'
+
+
+# for options see - https://github.com/teampoltergeist/poltergeist#Customization
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {:inspector => true})
+#, :js_errors => false})
+end
+
+# :js_errors => false
+# :window_size => [800,600]
+
+session = Capybara::Session.new(:selenium)#:poltergeist
+session.visit "https://beta.freefeed.net/"
+
+Wait.until!("login box with username is loaded") { session.has_content?("Username") }
+
+if session.has_content?("Username")
+  puts "All shiny, captain!"
+  session.save_screenshot('screenshot1.png')
+else
+#  puts "readyState is:",session.inspect
+
+  session.save_screenshot('screenshot2.png')
+  puts ":( no tagline fonud, possibly something's broken"
+  exit(-1)
+end
+
+
